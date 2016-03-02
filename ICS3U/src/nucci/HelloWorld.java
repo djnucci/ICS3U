95b70558136164e566dbd62dd59f2752e86feb75
Daniel Nucci <djnucci@hotmail.com>
2016-03-02 14:44:20 -0500

    First commit Wrote primary print statements

---
+++ ICS3U/src/nucci/HelloWorld.java
@@ -0,0 +1,19 @@
+package nucci;
+
+/*
+ * HelloWorld.java
+ * This program writes Hello World in the console
+ * Daniel Nucci
+ * March 2nd, 2016
+ */
+
+public class HelloWorld {
+
+	// FIXME
+	public static void main(String[] args) {
+		// This is to print to the console "hello world"
+		System.out.println("Hello World!");
+		System.out.println(":)");
+
+	}
+}
