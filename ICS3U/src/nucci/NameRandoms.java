67b08ead3649272f5dc7563cd471281edf886b5d
Daniel Nucci <djnucci@hotmail.com>
2016-03-23 14:18:40 -0400

    Finished NameRandoms

---
+++ ICS3U/src/nucci/NameRandoms.java
@@ -0,0 +1,33 @@
+package nucci;
+
+import java.util.Scanner;
+
+/*
+ * NameRandoms.java
+ * this program asks for a number then 
+ * Daniel Nucci
+ * March 23th, 2016
+ */
+
+public class NameRandoms {
+
+	public static void main(String[] args) {
+
+		Scanner read = new Scanner(System.in);
+
+		//variables
+		String userName;
+		int chance = (int) (Math.random() * 10) + 1;
+
+		//user input
+		System.out.println("What is your name?");
+		userName = read.nextLine();
+
+		//print the information
+		if (chance <= 3) {
+			System.out.println("I hate the name " + userName + ".");
+		} else {
+			System.out.println("I love the name " + userName + ".");
+		}
+	}
+}
