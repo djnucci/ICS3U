1197adfbb91b8cd45aebebd6fa46883bebb65c47
Daniel Nucci <djnucci@hotmail.com>
2016-03-29 14:23:39 -0400

    added replayablility

--- ICS3U/src/nucci/TwoDice.java
+++ ICS3U/src/nucci/TwoDice.java
@@ -1,5 +1,7 @@
 package nucci;
 
+import java.util.Scanner;
+
 /*
  * NameRandoms.java
  * this program rolls two dice for each "person" then compares 
@@ -10,31 +12,45 @@ package nucci;
 public class TwoDice {
 
 	public static void main(String[] args) {
+
+		Scanner read = new Scanner(System.in);
 		
-		//make two dice
-		int dieOne = (int) (Math.random() * 6) + 1;
-		int dieTwo = (int) (Math.random() * 6) + 1;
+		boolean isFinished = false;
+		String finish;
 		
-		//add the two
-		int userResult = dieOne + dieTwo;
-		
-		//do the same for the computer
-		dieOne = (int) (Math.random() * 6) + 1;
-		dieTwo = (int) (Math.random() * 6) + 1;
-		
-		//add the two
-		int aiResult = dieOne + dieTwo;
-		
-		//determine winner
-		if (userResult > aiResult) {
-			System.out.println("You won: " + userResult + " to " + aiResult);
-		}
-		else if (userResult < aiResult) {
-			System.out.println("You lost: " + aiResult + " to " + userResult);
-		}
-		else {
-			System.out.println("We tied: " + aiResult + " to " + userResult);
+		while (!isFinished) {
+			// make two dice
+			int dieOne = (int) (Math.random() * 6) + 1;
+			int dieTwo = (int) (Math.random() * 6) + 1;
+
+			// add the two
+			int userResult = dieOne + dieTwo;
+
+			// do the same for the computer
+			dieOne = (int) (Math.random() * 6) + 1;
+			dieTwo = (int) (Math.random() * 6) + 1;
+
+			// add the two
+			int aiResult = dieOne + dieTwo;
+
+			// determine winner
+			if (userResult > aiResult) {
+				System.out.println("You won: " + userResult + " to " + aiResult);
+			} else if (userResult < aiResult) {
+				System.out.println("You lost: " + aiResult + " to " + userResult);
+			} else {
+				System.out.println("We tied: " + aiResult + " to " + userResult);
+			}
+			System.out.println("Continue: y/n ?");
+			finish = read.nextLine();
+			
+			if (finish.equals("y")) {
+				isFinished = false;
+			}
+			else {
+				isFinished = true;
+				
+			}
 		}
 	}
-
 }
