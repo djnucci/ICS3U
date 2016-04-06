08944e6cae74d4df95bc63bc86f725ab0517c10f
Daniel Nucci <djnucci@hotmail.com>
2016-04-06 14:46:57 -0400

    Starting mechanics

--- ICS3U/src/nucci/AdventureGame.java
+++ ICS3U/src/nucci/AdventureGame.java
@@ -1,5 +1,7 @@
 package nucci;
 
+import hsa_new.Console;
+
 /** 
  * @author Daniel
  * @author Jitesh
@@ -11,6 +13,12 @@ public class AdventureGame {
 
 	public static void main(String[] args) {
 
+		Console log = new Console(50, 150);
+		
+		String[][] userInput = new String[2][10];
+		int gold;
+		
+		userInput[1][0] = log.readString();
 		
 		
 		
