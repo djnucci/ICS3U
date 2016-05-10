06befc60221214488a3aa7c5bd62875776060a30
Daniel Nucci <djnucci@hotmail.com>
2016-05-09 21:14:50 -0400

    finished Word game, little to no bugs found at this time

--- ICS3U/src/nucci/WordGuessingGame.java
+++ ICS3U/src/nucci/WordGuessingGame.java
@@ -9,7 +9,7 @@ public class WordGuessingGame {
 		String[] words = new String[]{"gyro", "do", "you", "often", "need", "to", "take", "spreadsheet", "of", "data", "and", "convert", "comma", "list", "special", "philatelist"};
 		Scanner read = new Scanner(System.in);
 		int randomWord = (int) (Math.random() * 16);
-		boolean dashes = false;
+		boolean dashes = true;
 
 		char[] wordDashes = new char[words[randomWord].length()];
 		System.out.println("Word Guessing Game...");
@@ -18,32 +18,52 @@ public class WordGuessingGame {
 			wordDashes[i] = '-';
 		}
 
-		for (int i = 0; i < words[randomWord].length(); i++) {
-			System.out.print(wordDashes[i]);
-		}
-		String userGuess = "thisisnotnull";
+		char[] wordOfWords = words[randomWord].toCharArray();
+		String userGuess = "#";
 		String guessWord;
 
-		while (!userGuess.equalsIgnoreCase("!") && !dashes) {
-			for (int i = 0; i < wordDashes[i]; i++) {
+		while (!userGuess.equalsIgnoreCase("!") && dashes) {
+			
+			for(int i = 0; i < wordDashes.length; i++){
+				if(wordOfWords[i] == userGuess.charAt(0)){
+					wordDashes[i] = userGuess.charAt(0);
+				}
+				
+				System.out.print(wordDashes[i]);
+				
+			}
+			
+			for (int i = 0; i < wordDashes.length; i++) {
 				if (wordDashes[i] == '-') {
-					dashes = false;
+					dashes = true;
 					break;
 				}
 				if (wordDashes[i] != '-') {
-					dashes = true;
+					dashes = false;
 				}
 			}
-			System.out.println("");
-			System.out.println("Enter a letter (or a ! to guess the word)");
+			System.out.println();
+			if(dashes){
+			System.out.print("Enter a letter (or a ! to guess the word):");
+			
 			userGuess = read.nextLine();
+			}
 		}
 
 		if (!dashes) {
-			System.out.println("You guessed the word, it was " + words[randomWord]);
+			System.out.println("Congrats, you won!");
+			System.out.println("You guessed the word, it was: " + "'" + words[randomWord] + "'");
 		} else {
 			System.out.println("What is your guess for the word?");
 			guessWord = read.nextLine();
+			if(guessWord.equalsIgnoreCase(words[randomWord])){
+				System.out.println("Congrats, you won!");
+				System.out.println("You guessed the word, it was: " + "'" + words[randomWord] + "'");
+			}
+			else{
+				System.out.println("You failed the task.");
+				System.out.println("You did not guess the word, it was: " + "'" + words[randomWord] + "'");
+			}
 		}
 	}
 
