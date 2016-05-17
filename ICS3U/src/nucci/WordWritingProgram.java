5e8b584035b53f6cb9e3d37d6b91469ddc38ccca
Daniel Nucci <djnucci@hotmail.com>
2016-05-17 15:05:36 -0400

    mississppi writing program

---
+++ ICS3U/src/nucci/WordWritingProgram.java
@@ -0,0 +1,77 @@
+package nucci;
+
+public class WordWritingProgram {
+
+	public static void main(String[] args) {
+		drawWord("MISSISSIPPI");
+	}
+
+	public static void drawWord(String word){
+		
+	}
+	public static void drawA(){
+		System.out.println("    A    ");//nine by five
+		System.out.println("   A A   ");
+		System.out.println("  AAAAA  ");
+		System.out.println(" A     A ");
+		System.out.println("A       A");
+		System.out.println();
+	}
+	public static void drawB(){
+		System.out.println("BBBBBB   ");//nine by five
+		System.out.println("BB   BB  ");
+		System.out.println("BBBBBB   ");
+		System.out.println("BB   BB  ");
+		System.out.println("BBBBBB   ");
+		System.out.println();
+	}
+	public static void drawC(){
+		System.out.println("  CCCCCC ");//nine by five
+		System.out.println(" CC      ");
+		System.out.println("CC       ");
+		System.out.println(" CC      ");
+		System.out.println("  CCCCCC ");
+		System.out.println();
+	}
+	public static void drawD(){
+		System.out.println("DDDDD    ");//nine by five
+		System.out.println("DD  DD   ");
+		System.out.println("DD   DD  ");
+		System.out.println("DD  DD   ");
+		System.out.println("DDDDD    ");
+		System.out.println();
+	}
+	public static void drawE(){
+		System.out.println("EEEEEEE  ");//nine by five
+		System.out.println("EE       ");
+		System.out.println("EEEEEE   ");
+		System.out.println("EE       ");
+		System.out.println("EEEEEEE  ");
+		System.out.println();
+	}
+	public static void drawF(){
+		System.out.println("FFFFFFFF ");//nine by five
+		System.out.println("FF       ");
+		System.out.println("FFFFF    ");
+		System.out.println("FF       ");
+		System.out.println("FF       ");
+		System.out.println();
+	}
+	public static void drawG(){
+		System.out.println(" GGGGGGG ");//nine by five
+		System.out.println("G       G");
+		System.out.println("G   GGG  ");
+		System.out.println("G      GG");
+		System.out.println(" GGGGGGG ");
+		System.out.println();
+	}
+	public static void drawH(){
+		System.out.println("HH     HH");//nine by five
+		System.out.println("HH     HH");
+		System.out.println("HHHHHHHHH");
+		System.out.println("HH     HH");
+		System.out.println("HH     HH");
+		System.out.println();
+	}
+
+}
