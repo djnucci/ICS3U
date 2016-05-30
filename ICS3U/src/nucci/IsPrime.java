81710a744c6c3f7f232408f256d0ef3d341ff6a4
Daniel Nucci <djnucci@hotmail.com>
2016-05-30 14:51:05 -0400

    added java docs

--- ICS3U/src/nucci/IsPrime.java
+++ ICS3U/src/nucci/IsPrime.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class IsPrime {
 	// global variable
 	public static int x = 0;
-
+	/**
+	 * this is the main method
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// scanner
 		Scanner scn = new Scanner(System.in);
@@ -22,7 +25,16 @@ public class IsPrime {
 		}
 
 	}
-
+	
+	/**
+	 * Determines if a number is divisable for all numbers below it
+	 * @param x
+	 * 			- int first number
+	 * @param i
+	 * 			- int second number
+	 * @return
+	 * 			boolean true = divisable, false = not divisable
+	 */
 	public static boolean isDivisible(int x, int i) {
 		if (x % i != 0) {
 			return false;
@@ -30,6 +42,15 @@ public class IsPrime {
 
 		return true;
 	}
+	
+	/**
+	 * Determmines if a number is prime
+	 * 
+	 * @param x 
+	 * 			- int number that is being checked if it is prime
+	 * @return 
+	 * 			boolean true = prime number, false = not prime number
+	 */
 	public static boolean isPrime(int x) {
 		if (x == 0 || x == 1) {
 			return (false);
